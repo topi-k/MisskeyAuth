c1e8312e578e8445901bdae3a6fbcb9999f8ddc1
topi <topi1999@oregano.ocn.ne.jp>
2023-02-17 01:36:26 +0900

    Update MiAuth.php
    Commit

--- src/MiAuth.php
+++ src/MiAuth.php
@@ -10,51 +10,113 @@ use CURLFile;
 class MiAuth
 {
 
-    public $instance = "misskey.io";
-    
+    private $instance = "misskey.io";
+    private $uuid = null;
+
     private $last_result_code = null;
     private $attempts = 0;
     private $max_attempts = 5;
     private $token = null;
 
-    private function makeRequest($method, $instance, $endpoint, $param) {
+    private $connection = null;
+
+    public function get($endpoint, $param)
+    {
+        if ($this->isSetToken()) {
+            $token = ["i" => $this->token];
+            $param = array_merge($param, $token);
+        }
+        $results = $this->makeRequest("GET", $endpoint, $param);
+        return $results;
+    }
+
+    public function post($endpoint, $param)
+    {
+        if ($this->isSetToken()) {
+            $token = ["i" => $this->token];
+            $param = array_merge($param, $token);
+        }
+        $results = $this->makeRequest("POST", $endpoint, $param);
+        return $results;
+    }
+
+    private function drive($endpoint, $param)
+    {
+        if ($this->isSetToken()) {
+            $token = ["i" => $this->token];
+            $param = array_merge($param, $token);
+        }
+    }
+
+    public function generateAuthURI($name, $callback, $permission): string
+    {
+        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
+        $query =  http_build_query(
+            array(
+                "name" => $name,
+                'callback' => $callback,
+                'permission' => $permission
+            ),
+        );
+        $query = preg_replace('/%5B(\d+?)%5D/', '', $query);
+        return "https://" . $this->instance . "/miauth/" . Util::GenerateUUID() . "?" . $query;
+    }
+
+    public function getAccessToken()
+    {
+        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
+        if (!$this->isSetUUID()) throw new Exception("UUID cannot be empty.");
+        $results = $this->get("/miauth/" . $this->uuid . "/check", []);
+        return $results->token;
+    }
+
+    private function makeRequest($method, $endpoint, $param)
+    {
+        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
         $options = array(
             CURLOPT_URL => "https://" . $this->instance . "/api/" . $endpoint,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => $param,
+            CURLOPT_POSTFIELDS => json_encode($param),
         );
-
-        switch($method){
+        var_dump($options);
+        switch ($method) {
             case "POST":
-                $options .= array(CURLOPT_HTTPHEADER => array('Content-Type: multipart/form-data'));
+                $options += [CURLOPT_HTTPHEADER => ['Content-Type: multipart/form-data']];
                 break;
             case "GET":
-                $options .= array(CURLOPT_HTTPHEADER => array('Content-Type: application/json'));  
+                $options += [CURLOPT_HTTPHEADER => ['Content-Type: application/json']];
                 break;
         }
 
         $this->resetLastResultCode();
         $this->resetAttemptsCount();
 
-        return request($method, $instance, $endpoint, $options)
+        return $this->request($method, $endpoint, $options);
     }
-    
-    private function request($method, $instance, $endpoint, $options) {
+
+    private function request($method, $endpoint, $options)
+    {
+        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
         do {
             $this->connection = curl_init();
+            var_dump($options);
             curl_setopt_array($this->connection, $options);
             $results = curl_exec($this->connection);
             $results = json_decode($results);
         } while ($this->requestAvailable());
+
+        return $results;
     }
 
-    private function requestAvailable() {
-        return getHttpCode() > 500 || getHttpCode() == 200;
+    private function requestAvailable()
+    {
+        return $this->getHttpCode() > 500;
     }
 
 
 
-    private function getHttpCode() {
+    public function getHttpCode(): int
+    {
         $code = curl_getinfo($this->connection, CURLINFO_RESPONSE_CODE);
         if ($code) {
             return $code;
@@ -63,22 +125,53 @@ class MiAuth
         }
     }
 
-    public function getLastResultCode(): int {
+    public function getLastResultCode(): int
+    {
         return $this->last_result_code;
     }
 
-    private function resetLastResultCode(): void {
+    private function resetLastResultCode(): void
+    {
         $this->last_result_code = null;
         return;
     }
 
-    private function resetAttemptsCount(): void {
+    private function resetAttemptsCount(): void
+    {
         $this->attempts = 0;
         return;
     }
 
-    public function setUserToken($token): void {
+    public function setUserToken($token): void
+    {
         $this->token = $token;
         return;
+    }
+
+    public function setInstance($instance): void
+    {
+        $this->instance = $instance;
+        return;
+    }
+
+    public function setUUID($uuid): void
+    {
+        $this->uuid = $uuid;
+        return;
+    }
+
+    private function isSetToken(): bool
+    {
+        return isset($this->token);
+    }
+
+    private function isSetInstance(): bool
+    {
+        return isset($this->instance);
+    }
+
+    private function isSetUUID(): bool
+    {
+        return isset($this->uuid);
     }
 }
