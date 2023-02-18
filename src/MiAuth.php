e5989971b02c108fb9e53b8ab7089470522af654
topi <topi1999@oregano.ocn.ne.jp>
2023-02-18 20:45:26 +0900

    Update MiAuth.php

--- src/MiAuth.php
+++ src/MiAuth.php
@@ -13,14 +13,15 @@ class MiAuth
     private $instance = "misskey.io";
     private $uuid = null;
 
-    private $last_result_code = null;
     private $attempts = 0;
     private $max_attempts = 5;
     private $token = null;
 
+    private $last_result_code = null;
+
     private $connection = null;
 
-    public function get($endpoint, $param)
+    public function get($endpoint, $param = [])
     {
         if ($this->isSetToken()) {
             $token = ["i" => $this->token];
@@ -30,22 +31,27 @@ class MiAuth
         return $results;
     }
 
-    public function post($endpoint, $param)
+    public function post($endpoint, $param = [], $file = null)
     {
         if ($this->isSetToken()) {
             $token = ["i" => $this->token];
             $param = array_merge($param, $token);
         }
-        $results = $this->makeRequest("POST", $endpoint, $param);
+
+        $results = $this->makeRequest("POST", $endpoint, $param, isset($file));
         return $results;
     }
 
-    private function drive($endpoint, $param)
+    public function UploadtoDrive($param = [], $file)
     {
         if ($this->isSetToken()) {
             $token = ["i" => $this->token];
             $param = array_merge($param, $token);
         }
+        if (!file_exists($file)) throw new Exception("File not found.");
+        
+        $results = $this->makeRequest("POST","drive/files/create",[],$file);
+        return $results;
     }
 
     public function generateAuthURI($name, $callback, $permission): string
@@ -67,34 +73,44 @@ class MiAuth
         if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
         if (!$this->isSetUUID()) throw new Exception("UUID cannot be empty.");
         $results = $this->get("/miauth/" . $this->uuid . "/check", []);
-        return $results->token;
+        if (isset($results->token)) {
+            return $results->token;
+        } else {
+            return null;
+        }
     }
 
-    private function makeRequest($method, $endpoint, $param)
+    private function makeRequest($method, $endpoint, $param, $file = null)
     {
         if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
         $options = array(
             CURLOPT_URL => "https://" . $this->instance . "/api/" . $endpoint,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => json_encode($param),
         );
-        var_dump($options);
         switch ($method) {
             case "POST":
-                $options += [CURLOPT_HTTPHEADER => ['Content-Type: multipart/form-data']];
+                if ($file) {
+                    $cfile = new CURLFile($file,'image/jpeg','test_name');
+                    $param += ['file' => $cfile];
+                    $options += [CURLOPT_HTTPHEADER => ['Content-Type: multipart/form-data']];
+                } else {
+                    $options += [CURLOPT_HTTPHEADER => ['Content-Type: application/json']];
+                }
                 break;
             case "GET":
                 $options += [CURLOPT_HTTPHEADER => ['Content-Type: application/json']];
                 break;
         }
+        $options += [CURLOPT_POSTFIELDS => json_encode($param)];
 
+        var_dump($options);
         $this->resetLastResultCode();
         $this->resetAttemptsCount();
 
-        return $this->request($method, $endpoint, $options);
+        return $this->request($options);
     }
 
-    private function request($method, $endpoint, $options)
+    private function request($options)
     {
         if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
         do {
@@ -103,6 +119,8 @@ class MiAuth
             curl_setopt_array($this->connection, $options);
             $results = curl_exec($this->connection);
             $results = json_decode($results);
+            $this->attempts++;
+            $this->last_result_code = curl_getinfo($this->connection, CURLINFO_RESPONSE_CODE);
         } while ($this->requestAvailable());
 
         return $results;
@@ -110,19 +128,7 @@ class MiAuth
 
     private function requestAvailable()
     {
-        return $this->getHttpCode() > 500;
-    }
-
-
-
-    public function getHttpCode(): int
-    {
-        $code = curl_getinfo($this->connection, CURLINFO_RESPONSE_CODE);
-        if ($code) {
-            return $code;
-        } else {
-            return null;
-        }
+        return $this->attempts < $this->max_attempts && $this->getLastResultCode() >= 500;
     }
 
     public function getLastResultCode(): int
