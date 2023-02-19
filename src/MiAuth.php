9c7763376ae1d034ce5ff83d5ce17690f3aa1f99
topi <topi1999@oregano.ocn.ne.jp>
2023-02-19 12:39:22 +0900

    Commit

--- src/MiAuth.php
+++ src/MiAuth.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Topi\MisskeyAuth;
 
-use Exception;
 use CURLFile;
+
+class MisskeyAuthException extends \Exception
+{
+}
 
 class MiAuth
 {
@@ -16,16 +19,16 @@ class MiAuth
     private $attempts = 0;
     private $max_attempts = 5;
     private $token = null;
+    private $timeout_ms = 10000;
 
     private $last_result_code = null;
-
     private $connection = null;
 
     public function get($endpoint, $param = [])
     {
         if ($this->isSetToken()) {
             $token = ["i" => $this->token];
-            $param = array_merge($param, $token);
+            $param = array_merge($token,$param);
         }
         $results = $this->makeRequest("GET", $endpoint, $param);
         return $results;
@@ -35,7 +38,7 @@ class MiAuth
     {
         if ($this->isSetToken()) {
             $token = ["i" => $this->token];
-            $param = array_merge($param, $token);
+            $param = array_merge($token,$param);
         }
 
         $results = $this->makeRequest("POST", $endpoint, $param, isset($file));
@@ -46,17 +49,17 @@ class MiAuth
     {
         if ($this->isSetToken()) {
             $token = ["i" => $this->token];
-            $param = array_merge($param, $token);
+            $param = array_merge($token,$param);
         }
-        if (!file_exists($file)) throw new Exception("File not found.");
-        
-        $results = $this->makeRequest("POST","drive/files/create",[],$file);
+        if (!file_exists($file)) throw new MisskeyAuthException("File not found.");
+
+        $results = $this->makeRequest("POST", "drive/files/create", $param, $file);
         return $results;
     }
 
     public function generateAuthURI($name, $callback, $permission): string
     {
-        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
+        if (!$this->isSetInstance()) throw new MisskeyAuthException("Instance cannot be empty.");
         $query =  http_build_query(
             array(
                 "name" => $name,
@@ -70,8 +73,8 @@ class MiAuth
 
     public function getAccessToken()
     {
-        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
-        if (!$this->isSetUUID()) throw new Exception("UUID cannot be empty.");
+        if (!$this->isSetInstance()) throw new MisskeyAuthException("Instance cannot be empty.");
+        if (!$this->isSetUUID()) throw new MisskeyAuthException("UUID cannot be empty.");
         $results = $this->get("/miauth/" . $this->uuid . "/check", []);
         if (isset($results->token)) {
             return $results->token;
@@ -82,16 +85,19 @@ class MiAuth
 
     private function makeRequest($method, $endpoint, $param, $file = null)
     {
-        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
+        if (!$this->isSetInstance()) throw new MisskeyAuthException("Instance cannot be empty.");
         $options = array(
             CURLOPT_URL => "https://" . $this->instance . "/api/" . $endpoint,
             CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT_MS => $this->timeout_ms
         );
+        
         switch ($method) {
             case "POST":
                 if ($file) {
-                    $cfile = new CURLFile($file,'image/jpeg','test_name');
-                    $param += ['file' => $cfile];
+                    if (!is_readable($file)) throw new MisskeyAuthException("File is not readable.");
+                    $file = new CURLFile($file,'image/jpeg','file');
+                    $param += ['file' => $file];
                     $options += [CURLOPT_HTTPHEADER => ['Content-Type: multipart/form-data']];
                 } else {
                     $options += [CURLOPT_HTTPHEADER => ['Content-Type: application/json']];
@@ -101,9 +107,8 @@ class MiAuth
                 $options += [CURLOPT_HTTPHEADER => ['Content-Type: application/json']];
                 break;
         }
-        $options += [CURLOPT_POSTFIELDS => json_encode($param)];
+        $options += [CURLOPT_POSTFIELDS => $param];
 
-        var_dump($options);
         $this->resetLastResultCode();
         $this->resetAttemptsCount();
 
@@ -112,17 +117,22 @@ class MiAuth
 
     private function request($options)
     {
-        if (!$this->isSetInstance()) throw new Exception("Instance cannot be empty.");
+        if (!$this->isSetInstance()) throw new MisskeyAuthException("Instance cannot be empty.");
         do {
             $this->connection = curl_init();
-            var_dump($options);
             curl_setopt_array($this->connection, $options);
             $results = curl_exec($this->connection);
-            $results = json_decode($results);
+            if (curl_errno($this->connection) > 0) {
+                $error = curl_error($this->connection);
+                $errorNo = curl_errno($this->connection);
+                curl_close($this->connection);
+                throw new MisskeyAuthException($error, $errorNo);
+            }
             $this->attempts++;
             $this->last_result_code = curl_getinfo($this->connection, CURLINFO_RESPONSE_CODE);
         } while ($this->requestAvailable());
 
+        $results = json_decode($results);
         return $results;
     }
 
@@ -157,6 +167,12 @@ class MiAuth
     public function setInstance($instance): void
     {
         $this->instance = $instance;
+        return;
+    }
+
+    public function setTimeout($timeout_ms): void
+    {
+        $this->timeout_ms = $timeout_ms;
         return;
     }
 
